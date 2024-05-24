337bede4c7089278c1edfe01def6e79f12957e85
Ye Zin Wai <yzwai@myseneca.ca>
2024-05-24 12:19:10 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Ye Zin Wai
+#Author ID: 133310227
+#Date Created: 2024/05/24
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
