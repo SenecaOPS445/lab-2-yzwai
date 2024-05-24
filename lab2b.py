c7482d86fcebf3494cccbfd66b7354fd73a7f5f4
Ye Zin Wai <yzwai@myseneca.ca>
2024-05-24 10:58:01 -0400

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+#name = 'Jon'
+#age = 20
+#print("Name: " + name)
+#print("Age: " + str(age))
+#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+name = input("Name: ")
+#print("Name: " + name)
+age = input("Age: ")
+#print("Age: " + age)
+print("Hi " + name + ', you are ' + age + ' years old.')
