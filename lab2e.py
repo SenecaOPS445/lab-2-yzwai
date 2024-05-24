fb202ab269674b4496df0f32100b021fd7c64d51
Ye Zin Wai <yzwai@myseneca.ca>
2024-05-24 12:02:38 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Ye Zin Wai
+#Author ID: 133310227
+#Date Created: 2024/05/24
+
+count = 10
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
+
