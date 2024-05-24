a833c15a0e4238adde41e2c72d45b5345298381c
Ye Zin Wai <yzwai@myseneca.ca>
2024-05-24 13:36:41 -0400

    Added lab2g

---
+++ lab2g.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#Author: Ye Zin Wai
+#Author ID: 133310227
+#Date Created: 2024/05/24
+
+import sys
+
+if len(sys.argv) == 1:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
